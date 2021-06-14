b949728e7699e5997dd7d727316f73d06bcdd1c6
Ilya Agafonov <60569587+agafonovilya@users.noreply.github.com>
2021-06-14 20:40:47 +0300

    Espresso

--- app/src/androidTestForUsage/java/com/geekbrains/tests/MainActivityEspressoTest.kt
+++ app/src/androidTestForUsage/java/com/geekbrains/tests/MainActivityEspressoTest.kt
@@ -1,7 +1,11 @@
 package com.geekbrains.tests
 
+import android.content.Context
 import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
@@ -10,20 +14,66 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.geekbrains.tests.view.search.MainActivity
+import junit.framework.TestCase
 import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
+
 @RunWith(AndroidJUnit4::class)
 class MainActivityEspressoTest {
 
     private lateinit var scenario: ActivityScenario<MainActivity>
+    //private lateinit var context: Context
+
 
     @Before
     fun setup() {
         scenario = ActivityScenario.launch(MainActivity::class.java)
+       // context = ApplicationProvider.getApplicationContext()
+    }
+
+    @Test
+    fun activity_AssertNotNull() {
+        scenario.onActivity {
+            TestCase.assertNotNull(it)
+        }
+    }
+
+    @Test
+    fun activity_IsResumed() {
+        TestCase.assertEquals(Lifecycle.State.RESUMED, scenario.state)
+    }
+
+    @Test
+    fun activityEditText_NotNull() {
+        scenario.onActivity {
+            val searchEditText = it.findViewById<TextView>(R.id.searchEditText)
+            TestCase.assertNotNull(searchEditText)
+        }
+    }
+
+    @Test
+    fun activityEditText_IsDisplayed() {
+        onView(withId(R.id.searchEditText)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun activityEditText_IsCompletelyDisplayed() {
+        onView(withId(R.id.searchEditText)).check(matches(isCompletelyDisplayed()))
+    }
+
+    @Test
+    fun activityButtons_AreEffectiveVisible() {
+        onView(withId(R.id.toDetailsActivityButton)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+    }
+
+    @Test
+    fun activityButton_IsWorking() {
+        onView(withId(R.id.toDetailsActivityButton)).perform(click())
+        //todo()
     }
 
     @Test
@@ -32,6 +82,7 @@ class MainActivityEspressoTest {
         onView(withId(R.id.searchEditText)).perform(replaceText("algol"), closeSoftKeyboard())
         onView(withId(R.id.searchEditText)).perform(pressImeActionButton())
 
+        
         if (BuildConfig.TYPE == MainActivity.FAKE) {
             onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 42")))
         } else {
